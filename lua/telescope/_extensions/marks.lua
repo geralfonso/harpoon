bb7a30627b4ec8d35a113767d4424ebe7f97c669
German Alfonso <geralfonsocastillo@gmail.com>
2023-07-18 16:56:57 -0500

    fix truncated entries

--- lua/telescope/_extensions/marks.lua
+++ lua/telescope/_extensions/marks.lua
@@ -31,7 +31,6 @@ local generate_new_finder = function()
                 separator = " - ",
                 items = {
                     { width = 2 },
-                    { width = 50 },
                     { remaining = true },
                 },
             })
